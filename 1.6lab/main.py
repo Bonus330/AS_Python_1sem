f04ff600e661e4d196a363c09fa3af263613fcb9
Bonus330 <danil.zinchenko.07@bk.ru>
2025-12-22 18:56:29 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
+if "Danil Zinchenko" == "7":
     pass
+s = input("Введите строку: ")
+if len(s) <= 1:
+    result = s
+else:
+    result = s[-1] + s[1:-1] + s[0]
+print(result)
