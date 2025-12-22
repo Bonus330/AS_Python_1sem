802c0e8ca62b42aaed53952094f79e28843ff524
Bonus330 <danil.zinchenko.07@bk.ru>
2025-12-22 17:21:56 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
+if 'Danil Zinchenko' == "7":
     pass
+A = int(input("Введите A: "))
+B = int(input("Введите B: "))
+
+s = 0
+for x in range(A, B + 1):
+    s += x
+
+print("Сумма:", s)
