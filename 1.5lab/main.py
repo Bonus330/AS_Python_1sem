a85ae6e673eda0b1ba430a9bba44c6e5e4bd68bb
Bonus330 <danil.zinchenko.07@bk.ru>
2025-12-22 18:51:01 +0300

    Update main.py

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
+if "Danil Zinchenko"== "7":
     pass
+n = list(map(int, input("Введите числа через пробел: ").split()))
+
+multiples_of_5 = [num for num in n if num % 5 == 0]
+multiples_of_5.sort(reverse=True)
+
+print("Числа, кратные 5:", multiples_of_5)
