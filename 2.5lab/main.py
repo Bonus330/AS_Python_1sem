4c4e759622614326e71a31d700cb725ae764858c
Bonus330 <danil.zinchenko.07@bk.ru>
2025-12-23 16:59:56 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,19 @@
 if __name__ == "__main__":
     pass
+def min_rec(arr, n):
+    if n == 1:
+        return arr[0]
+    min_of_rest = min_rec(arr, n - 1)
+    return min(arr[n - 1], min_of_rest)
+def main():
+    array1 = [3, 5, 2, 8, 1]
+    array2 = [10, -3, 0, 7, 4]
+    array3 = [9, 9, 9, 9, 9]
+    min1 = min_rec(array1, len(array1))
+    min2 = min_rec(array2, len(array2))
+    min3 = min_rec(array3, len(array3))
+    print(f"Минимальный элемент в первом массиве: {min1}")
+    print(f"Минимальный элемент во втором массиве: {min2}")
+    print(f"Минимальный элемент в третьем массиве: {min3}")
+if __name__ == "__main__":
+    main()
