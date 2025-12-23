dd6d785ecc4f49fa7ca05b2778867617ee0e1b10
Bonus330 <danil.zinchenko.07@bk.ru>
2025-12-23 16:24:52 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
+if 'Danil Zinchenko' == "7":
     pass
+with open('2.7.txt','r') as file:
+    count = 0
+    total_sum = 0
+    for line in file:
+        number = int(line.strip())
+        count += 1
+        total_sum += number
+print("Количество чисел:", count)
+print("Сумма чисел:", total_sum)
