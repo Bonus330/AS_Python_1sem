43b47fd33dbb99a5032c4b021efc2ebfa966cfe2
Bonus330 <danil.zinchenko.07@bk.ru>
2025-12-23 16:45:49 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
+if 'Danil Zinchenko' == "7":
     pass
+  with open('f.txt', 'r') as file:
+    first_two_chars = file.read(2)
+    if first_two_chars[0].isdigit() and first_two_chars[1].isdigit():
+        s = int(first_two_chars)
+        if s % 2 == 0:
+            print(f"Первые два символа '{first_two_chars}' образуют четное число")
+        else:
+            print(f"Первые два символа '{first_two_chars}' образуют нечетное число")
+    else:
+        print("Первые два символа не являются цифрами")  
